04a7b2a71ffb5ed12ceadc253f40a9ab2d62426e
Nellis,Dean <dean.nellis@lexmark.com>
2021-01-16 00:31:14 -0500

    Added landing page sections

--- src/components/LandingHeader.js
+++ src/components/LandingHeader.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import { LogoIcon } from '../utils/icons';
 
 const LandingHeader = () => {
 	return (
 		<header className="landing-header">
-			<LogoIcon useGradient size={64} />
-			<h1>craft pixel</h1>
+			<div>
+				<LogoIcon useGradient size={64} />
+				<h1>craft pixel</h1>
+			</div>
+			<nav>
+				<Link to="sample-home">Sample Site</Link>
+				<a href="">Contact</a>
+			</nav>
 		</header>
 	);
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,54 @@
 import React from 'react';
+import { Controller, Scene } from 'react-scrollmagic';
 
 import LandingHeader from '../components/LandingHeader';
 import SEO from '../components/Seo';
 import AnimatedLogo from '../components/AnimatedLogo';
 import WithArrow from '../components/WithArrow';
+import SampleSiteCta from '../components/SampleSiteCta';
+import FastSection from '../components/FastSection';
+import SeoSection from '../components/SeoSection';
+import Footer from '../components/Footer';
 
 const IndexPage = () => {
 	return (
-		<main>
-			<SEO title="Craft Pixel Landing Page" />
-			<LandingHeader />
-			<div className="index__hero">
-				<section className="index__hero-cta">
-					<h1>
-						Let's craft your <br></br>website together
-					</h1>
-					<p>
-						{' '}
-						Your brand is bigger than a template or a theme. We have the design
-						and development expertise to help build your next site, store, blog,
-						or digital dreamscape.
-					</p>
-					<button className="button--large">
-						<WithArrow animate size={16}>
-							Let's Talk
-						</WithArrow>
-					</button>
-				</section>
-				<section>
-					<AnimatedLogo />
-				</section>
-			</div>
-		</main>
+		<Controller>
+			<Scene duration={600} offset={1000}>
+				{(progress, event) => (
+					<div className="scroll-handle">
+						<main>
+							<SEO title="Craft Pixel: Experts in Design and Development" />
+							<LandingHeader />
+							<div className="index__hero">
+								<section className="index__hero-cta">
+									<h1>
+										Let's craft your <br></br>website together
+									</h1>
+									<p>
+										{' '}
+										Your brand is bigger than a template or a theme. We have the
+										design and development expertise to help custom build your
+										next site, store, blog, or digital dreamscape.
+									</p>
+									<button className="button--large">
+										<WithArrow animate size={16}>
+											Let's Talk
+										</WithArrow>
+									</button>
+								</section>
+								<section>
+									<AnimatedLogo />
+								</section>
+							</div>
+							<SampleSiteCta />
+							<FastSection scrollState={event.state} />
+							<SeoSection />
+						</main>
+						<Footer />
+					</div>
+				)}
+			</Scene>
+		</Controller>
 	);
 };
 
